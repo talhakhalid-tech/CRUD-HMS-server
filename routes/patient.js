57ca0fbcfa9301c2167da71cf16bfdadcb1ed2c2
talhakhalid-tech <talhakhalid0323@gmail.com>
2021-06-10 14:20:24 +0500

    completed patient route

--- routes/patient.js
+++ routes/patient.js
@@ -29,63 +29,47 @@ router.post("/create", async (req, res) => {
   }
 });
 
-// router.delete("/delete", async (req, res) => {
-//   try {
-//     const sql = `DELETE FROM ward WHERE W_id = ${req.query.wId};`;
-//     mysql.query(sql, function (error, results) {
-//       if (error) throw error;
-//       res.status(202).json({ message: "Deleted Ward Data Successfully" });
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error });
-//   }
-// });
+router.delete("/delete", async (req, res) => {
+  try {
+    const sql = `DELETE FROM patient WHERE P_id = ${req.query.pId};`;
+    mysql.query(sql, function (error, results) {
+      if (error) throw error;
+      res.status(202).json({ message: "Deleted Patient Data Successfully" });
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
-// router.patch("/update", async (req, res) => {
-//   try {
-//     const { wId, wType, wFloor, wTotalBeds, wReservedBeds } = req.body;
-//     const sql = `UPDATE ward
-//     SET W_Type = '${wType}', W_Floor = '${wFloor}', W_TotalBeds='${wTotalBeds}', W_ReservedBeds= '${wReservedBeds}'
-//     WHERE W_id = ${wId};`;
-//     mysql.query(sql, function (error, results) {
-//       if (error) throw error;
-//       res.status(201).json({ message: "Updated Ward Data Successfully" });
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error });
-//   }
-// });
+router.patch("/update", async (req, res) => {
+  try {
+    const { pId, pName, pDisease, pBedNo, dId, aId, wId } = req.body;
+    const sql = `UPDATE patient
+    SET P_Name = '${pName}', P_Disease = '${pDisease}', P_BedNo='${pBedNo}', D_id= ${dId},A_id=${aId},W_id=${wId}
+    WHERE P_id = ${pId};`;
+    mysql.query(sql, function (error, results) {
+      if (error) throw error;
+      res.status(201).json({ message: "Updated Ward Data Successfully" });
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
-// router.get("/relatedPatients", async (req, res) => {
-//   try {
-//     const sql = `SELECT ward.W_id,patient.P_id,patient.P_Name,patient.P_Disease,patient.P_BedNo
-//     FROM ward
-//     RIGHT JOIN patient
-//     ON ward.W_id = patient.W_id
-//     WHERE patient.W_id = ${req.query.WId};`;
-//     mysql.query(sql, function (error, results) {
-//       if (error) throw error;
-//       res.status(200).json({ patients: results });
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error });
-//   }
-// });
-
-// router.get("/relatedNurses", async (req, res) => {
-//   try {
-//     const sql = `SELECT ward.W_id,nurse.N_id,nurse.N_Name,nurse.N_Position,nurse.N_CellNo,nurse.N_Shift
-//     FROM ward
-//     RIGHT JOIN nurse
-//     ON ward.W_id = nurse.W_id
-//     WHERE nurse.W_id = ${req.query.WId};`;
-//     mysql.query(sql, function (error, results) {
-//       if (error) throw error;
-//       res.status(200).json({ nurses: results });
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error });
-//   }
-// });
+router.get("/relatedNurses", async (req, res) => {
+  try {
+    const sql = `SELECT patient_nurse.P_id,nurse.N_id,nurse.N_Name,nurse.N_Position,nurse.N_CellNo,nurse.N_Shift
+    FROM patient_nurse
+    RIGHT JOIN nurse
+    ON patient_nurse.N_id = nurse.N_id
+    WHERE patient_nurse.P_id = ${req.query.PId};`;
+    mysql.query(sql, function (error, results) {
+      if (error) throw error;
+      res.status(200).json({ nurses: results });
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
